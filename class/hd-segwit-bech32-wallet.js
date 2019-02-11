0516dd2bd20e4d3b0e5178be70fd3799635b9d38
Overtorment <overtorment@gmail.com>
2019-02-11 00:46:51 +0000

    WIP

---
+++ class/hd-segwit-bech32-wallet.js
@@ -0,0 +1,250 @@
+import { AbstractHDWallet } from './abstract-hd-wallet';
+import { NativeModules } from 'react-native';
+import bitcoin from 'bitcoinjs-lib';
+import bip39 from 'bip39';
+import BigNumber from 'bignumber.js';
+import b58 from 'bs58check';
+import signer from '../models/signer';
+import { WatchOnlyWallet } from './watch-only-wallet';
+
+const { RNRandomBytes } = NativeModules;
+
+/**
+ * Converts zpub to xpub
+ *
+ * @param {String} zpub
+ * @returns {String} xpub
+ */
+function _zpubToXpub(zpub) {
+  let data = b58.decode(zpub);
+  data = data.slice(4);
+  data = Buffer.concat([Buffer.from('0488b21e', 'hex'), data]);
+
+  return b58.encode(data);
+}
+
+/**
+ * Creates Segwit Bech32 Bitcoin address
+ *
+ * @param hdNode
+ * @returns {String}
+ */
+function _nodeToBech32SegwitAddress(hdNode) {
+  const pubkeyBuf = hdNode.keyPair.getPublicKeyBuffer();
+  var scriptPubKey = bitcoin.script.witnessPubKeyHash.output.encode(bitcoin.crypto.hash160(pubkeyBuf));
+  var address = bitcoin.address.fromOutputScript(scriptPubKey);
+  return address;
+}
+
+/**
+ * HD Wallet (BIP39).
+ * In particular, BIP84 (Bech32 Native Segwit)
+ * @see https://github.com/bitcoin/bips/blob/master/bip-0084.mediawiki
+ */
+export class HDSegwitBech32Wallet extends AbstractHDWallet {
+  static type = 'HDsegwitBech32';
+  static typeReadable = 'HD SegWit (BIP84 Bech32 Native)';
+
+  allowSend() {
+    return true;
+  }
+
+  async generate() {
+    let that = this;
+    return new Promise(function(resolve) {
+      if (typeof RNRandomBytes === 'undefined') {
+        // CLI/CI environment
+        // crypto should be provided globally by test launcher
+        return crypto.randomBytes(32, (err, buf) => { // eslint-disable-line
+          if (err) throw err;
+          that.secret = bip39.entropyToMnemonic(buf.toString('hex'));
+          resolve();
+        });
+      }
+
+      // RN environment
+      RNRandomBytes.randomBytes(32, (err, bytes) => {
+        if (err) throw new Error(err);
+        let b = Buffer.from(bytes, 'base64').toString('hex');
+        that.secret = bip39.entropyToMnemonic(b);
+        resolve();
+      });
+    });
+  }
+
+  _getExternalWIFByIndex(index) {
+    return this._getWIFByIndex(false, index);
+  }
+
+  _getInternalWIFByIndex(index) {
+    return this._getWIFByIndex(true, index);
+  }
+
+  /**
+   * Get internal/external WIF by wallet index
+   * @param {Boolean} internal
+   * @param {Number} index
+   * @returns {*}
+   * @private
+   */
+  _getWIFByIndex(internal, index) {
+    const mnemonic = this.secret;
+    const seed = bip39.mnemonicToSeed(mnemonic);
+    const root = bitcoin.HDNode.fromSeedBuffer(seed);
+    const path = `m/84'/0'/0'/${internal ? 1 : 0}/${index}`;
+    const child = root.derivePath(path);
+
+    return child.keyPair.toWIF();
+  }
+
+  _getNodeAddressByIndex(node, index) {
+    index = index * 1; // cast to int
+    if (node === 0) {
+      if (this.external_addresses_cache[index]) return this.external_addresses_cache[index]; // cache hit
+    }
+
+    if (node === 1) {
+      if (this.internal_addresses_cache[index]) return this.internal_addresses_cache[index]; // cache hit
+    }
+
+    const xpub = _zpubToXpub(this.getXpub());
+    const hdNode = bitcoin.HDNode.fromBase58(xpub);
+    const address = _nodeToBech32SegwitAddress(hdNode.derive(node).derive(index));
+
+    if (node === 0) {
+      return (this.external_addresses_cache[index] = address);
+    }
+
+    if (node === 1) {
+      return (this.internal_addresses_cache[index] = address);
+    }
+  }
+
+  _getExternalAddressByIndex(index) {
+    return this._getNodeAddressByIndex(0, index);
+  }
+
+  _getInternalAddressByIndex(index) {
+    return this._getNodeAddressByIndex(1, index);
+  }
+
+  /**
+   * Returning zpub actually, not xpub. Keeping same method name
+   * for compatibility.
+   *
+   * @return {String} zpub
+   */
+  getXpub() {
+    if (this._xpub) {
+      return this._xpub; // cache hit
+    }
+    // first, getting xpub
+    const mnemonic = this.secret;
+    const seed = bip39.mnemonicToSeed(mnemonic);
+    const root = bitcoin.HDNode.fromSeedBuffer(seed);
+
+    const path = "m/84'/0'/0'";
+    const child = root.derivePath(path).neutered();
+    const xpub = child.toBase58();
+
+    // bitcoinjs does not support zpub yet, so we just convert it from xpub
+    let data = b58.decode(xpub);
+    data = data.slice(4);
+    data = Buffer.concat([Buffer.from('04b24746', 'hex'), data]);
+    this._xpub = b58.encode(data);
+
+    return this._xpub;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  async fetchTransactions() {
+    throw new Error('TODO');
+  }
+
+  createTx(utxos, amount, fee, address) {
+    for (let utxo of utxos) {
+      utxo.wif = this._getWifForAddress(utxo.address);
+    }
+
+    let amountPlusFee = parseFloat(new BigNumber(amount).plus(fee).toString(10));
+    return signer.createHDSegwitTransaction(
+      utxos,
+      address,
+      amountPlusFee,
+      fee,
+      this._getInternalAddressByIndex(this.next_free_change_address_index),
+    );
+  }
+
+  /**
+   * Derives from hierarchy, returns next free address
+   * (the one that has no transactions). Looks for several,
+   * gives up if none found, and returns the used one
+   *
+   * @return {Promise.<string>}
+   */
+  async getAddressAsync() {
+    // looking for free external address
+    let freeAddress = '';
+    let c;
+    for (c = 0; c < Math.max(5, this.usedAddresses.length); c++) {
+      if (this.next_free_address_index + c < 0) continue;
+      let address = this._getExternalAddressByIndex(this.next_free_address_index + c);
+      this.external_addresses_cache[this.next_free_address_index + c] = address; // updating cache just for any case
+      let WatchWallet = new WatchOnlyWallet();
+      WatchWallet.setSecret(address);
+      await WatchWallet.fetchTransactions();
+      if (WatchWallet.transactions.length === 0) {
+        // found free address
+        freeAddress = WatchWallet.getAddress();
+        this.next_free_address_index += c; // now points to _this one_
+        break;
+      }
+    }
+
+    if (!freeAddress) {
+      // could not find in cycle above, give up
+      freeAddress = this._getExternalAddressByIndex(this.next_free_address_index + c); // we didnt check this one, maybe its free
+      this.next_free_address_index += c + 1; // now points to the one _after_
+    }
+
+    return freeAddress;
+  }
+
+  /**
+   * Derives from hierarchy, returns next free CHANGE address
+   * (the one that has no transactions). Looks for several,
+   * gives up if none found, and returns the used one
+   *
+   * @return {Promise.<string>}
+   */
+  async getChangeAddressAsync() {
+    // looking for free internal address
+    let freeAddress = '';
+    let c;
+    for (c = 0; c < Math.max(5, this.usedAddresses.length); c++) {
+      if (this.next_free_change_address_index + c < 0) continue;
+      let address = this._getInternalAddressByIndex(this.next_free_change_address_index + c);
+      this.internal_addresses_cache[this.next_free_change_address_index + c] = address; // updating cache just for any case
+      let WatchWallet = new WatchOnlyWallet();
+      WatchWallet.setSecret(address);
+      await WatchWallet.fetchTransactions();
+      if (WatchWallet.transactions.length === 0) {
+        // found free address
+        freeAddress = WatchWallet.getAddress();
+        this.next_free_change_address_index += c; // now points to _this one_
+        break;
+      }
+    }
+
+    if (!freeAddress) {
+      // could not find in cycle above, give up
+      freeAddress = this._getExternalAddressByIndex(this.next_free_address_index + c); // we didnt check this one, maybe its free
+      this.next_free_address_index += c + 1; // now points to the one _after_
+    }
+
+    return freeAddress;
+  }
+}
